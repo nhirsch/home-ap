c91dfc3c43bf23ea5a58549c81d4c8bc788ecc7a
Pablo Martín De Natale <pablo.denatale@mercadolibre.com>
2012-08-21 18:07:08 -0300

    renombre algunas variables que estan poco claras

--- grails-app/controllers/autoplaza/HomeController.groovy
+++ grails-app/controllers/autoplaza/HomeController.groovy
@@ -1,91 +1,66 @@
 package autoplaza
 
-
-
 class HomeController {
-	
-	def Anuncios = []
-	
-    def index() {
-			
-			listadovip()  // generamos el listado VIP
-			["Anuncios":Anuncios]
-		 }
-	
-	
-	// metodo para generar el listado de los anuncios VIP para la seccion de la home
-	def listadovip()
-	{
-		
-		def APSErvicio = new APDataService()
-		def Dat = APSErvicio.procesarQuery("home")
-		
-		if (Dat != "No tenemos resultados") // solo para prueba revisar 
-		{
-		
-			def seqArray = Dat
-			int i = 0
-			seqArray.splitEachLine("%%%"){
-			
-			   it.each{ x ->
-				   def object = x.split("@@@")
-				   if (i == 0)
-				   {
-					 i=i+1
-				   }else
-					 {
-						 // es anuncio
-						 /*render "<br>mas...<br>"
-						 render "<br><b>modelo = </b>${object[0]}"
-						 render "<br><b>id =</b> ${object[1]}"
-						 render "<br><b>año = </b>${object[2]}"
-						 render "<br><b>marca =</b> ${object[3]}"
-						 render "<br><b>transmision = </b>${object[4]}"
-						 render "<br><b>precio =</b> ${object[5]}"
-						 render "<br><b>kilometraje = </b>${object[6]}"
-						 render "<br><b>color =</b> ${object[7]}"
-						 render "<br><b>definir = </b>${object[8]}"
-						 render "<br><b>estado =</b> ${object[9]}"
-						 render "<br><b>foto = </b>${object[10]}"
-						 render "<br><b>tipoanuncioID =</b> ${object[11]}"
-						 render "<br><b>version = </b>${object[12]}"
-						 */
 
-						 def Item = new  Anuncio(
-							 identificador:"${object[1]}",
-							 marca:"${object[3]}",
-							 modelo:"${object[0]}",
-							 urlFotoPrincipal:"${object[10]}",
-							 anio:"${object[2]}",
-							 precio:"${object[5]}",
-							 Estado:"${object[9]}"
-							 
-							 )
-						
-						Anuncios.add(Item)
-					 }
-				 
-			
-				   } // end each
-			
-			} // end split
-			
-		} // end if
-		
-		
-	}
-	
-	// metodo para generar los menus (navegadores: marca, precio, estado y año) para la seccion de la home
-	def menus()
-	{
-        
-	}	
-	
-	// genera los tag's de publicidad para la seccion de la home
-	def publicidad()
-	{
-		
-	}																																																																	
-	
-	
+    def Anuncios = []
+
+    def index() {
+
+        listadovip()  // generamos el listado VIP
+        ["Anuncios": Anuncios]
+    }
+
+    // metodo para generar el listado de los anuncios VIP para la seccion de la home
+    def listadovip() {
+
+        def APServicio = new APDataService()
+        def Dat = APServicio.procesarQuery("home")
+
+        if (Dat != "No tenemos resultados") // solo para prueba revisar
+        {
+
+            def seqArray = Dat
+            int i = 0
+            seqArray.splitEachLine("%%%") {
+
+                it.each { x ->
+                    def unAnuncio = x.split("@@@")
+                    if (i == 0) {
+                        i = i + 1
+                    } else {
+                        def Item = new Anuncio(
+                                identificador: "${unAnuncio[1]}",
+                                marca: "${unAnuncio[3]}",
+                                modelo: "${unAnuncio[0]}",
+                                urlFotoPrincipal: "${unAnuncio[10]}",
+                                anio: "${unAnuncio[2]}",
+                                precio: "${unAnuncio[5]}",
+                                Estado: "${unAnuncio[9]}"
+
+                        )
+
+                        Anuncios.add(Item)
+                    }
+
+
+                } // end each
+
+            } // end split
+
+        } // end if
+
+
+    }
+
+    // metodo para generar los menus (navegadores: marca, precio, estado y año) para la seccion de la home
+    def menus() {
+
+    }
+
+    // genera los tag's de publicidad para la seccion de la home
+    def publicidad() {
+
+    }
+
+
 }
